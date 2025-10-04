7ea964fcbe48ce8265bf4f06be9f75e9839fad44
phil-chen-0806 <cp18918509809@163.com>
2025-10-04 04:44:56 -0400

    2025.10.4 16：44 ...

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -1,15 +1,41 @@
 package com.project.back_end.mvc;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class DashboardController {
+
 
 // 1. Set Up the MVC Controller Class:
 //    - Annotate the class with `@Controller` to indicate that it serves as an MVC controller returning view names (not JSON).
 //    - This class handles routing to admin and doctor dashboard pages based on token validation.
 
-
+    @Autowired
+    private TokenService tokenService;
 // 2. Autowire the Shared Service:
 //    - Inject the common `Service` class, which provides the token validation logic used to authorize access to dashboards.
 
+    @GetMapping("/adminDashboard/{token}")
+    public String adminDashboard(@PathVariable int token) {
+        boolean isValid = tokenService.validateToken(token, "admin");
+        if(isValid){
+            return "admin/adminDashboard";
+        }
+        else{
+            return "redirect:/login";
+        }
+    }
+
+    @GetMapping("/doctorDashboard/{token}")
+    public String doctorDashboard(@PathVariable int token) {
+        boolean isValid = tokenService.validateToken(token, "doctor");
+        if(isValid){
+            return "doctor/doctorDashboard";
+        }
+        else{
+            return "redirect:/login";
+        }
+    }
 
 // 3. Define the `adminDashboard` Method:
 //    - Handles HTTP GET requests to `/adminDashboard/{token}`.
