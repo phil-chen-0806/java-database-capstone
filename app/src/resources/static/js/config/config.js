6cf9c293d5c29114a0bf9f8ebb4008dfeec1bc25
phil-chen-0806 <cp18918509809@163.com>
2025-10-05 20:50:08 -0400

    2025.10.6 8:49 build escape test

---
+++ app/src/resources/static/js/config/config.js
@@ -0,0 +1,3 @@
+//export const API_BASE_URL = "http://localhost:8080";
+
+export const API_BASE_URL = "https://cp1891850980-8080.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
--- app/src/test/java/com/project/back_end/BackEndApplicationTests.java
+++ app/src/test/java/com/project/back_end/BackEndApplicationTests.java
@@ -3,7 +3,12 @@ package com.project.back_end;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest
+@SpringBootTest(classes = BackEndApplication.class)
 class BackEndApplicationTests {
 
 	@Test
